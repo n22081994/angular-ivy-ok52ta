1f6c047ea20bd2f1838cc85687d72d7a60544d91
n22081994 <n22081994@gmail.com>
2022-01-09 10:44:54 -0500

    2 files modified

--- src/app/chat-list/chat-list.component.ts
+++ src/app/chat-list/chat-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Message {
+  me: boolean;
+  text: string;
+}
+
 @Component({
   selector: 'app-chat-list',
   templateUrl: './chat-list.component.html',
-  styleUrls: ['./chat-list.component.css']
+  styleUrls: ['./chat-list.component.css'],
 })
 export class ChatListComponent implements OnInit {
+  chats: Message[] = [
+    { me: true, text: 'What happened last night?' },
+    { me: false, text: 'You were drunk.' },
+    { me: true, text: "No I wasn't." },
+    {
+      me: false,
+      text: 'You were hugging an old man with a beard screaming "DUMBLEDORE YOU\'RE ALIVE!',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
