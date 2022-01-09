ca38a1234da8cf6ce86423fab42dfef18327a2a9
n22081994 <n22081994@gmail.com>
2022-01-09 08:15:49 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { ChatListComponent } from './chat-list/chat-list.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   declarations: [AppComponent, ChatListComponent],
   bootstrap: [AppComponent],
 })
