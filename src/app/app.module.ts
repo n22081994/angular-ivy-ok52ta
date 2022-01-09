4a5d2394aaaf0b69e63a005d616bb91f1901e3f4
n22081994 <n22081994@gmail.com>
2022-01-08 22:12:24 -0500

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ChatListComponent } from './chat-list/chat-list.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, ChatListComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
