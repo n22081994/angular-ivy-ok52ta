02810218909284578995ae41b5298e2a369500e1
n22081994 <n22081994@gmail.com>
2022-01-09 14:02:45 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent {
     const date = new Date();
     var hours = date.getHours();
     let minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
+    var ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     const min = minutes < 10 ? '0' + minutes : minutes;
