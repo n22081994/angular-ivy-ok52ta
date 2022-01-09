5c9a0a0b980bf007ff4c5c685b56749287bd81a4
n22081994 <n22081994@gmail.com>
2022-01-09 08:54:32 -0500

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { ChatListComponent } from './chat-list/chat-list.component';
@@ -12,11 +14,13 @@ import { ChatListComponent } from './chat-list/chat-list.component';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     FlexLayoutModule,
     MatIconModule,
     MatToolbarModule,
     MatButtonModule,
+    MatSidenavModule,
   ],
   declarations: [AppComponent, ChatListComponent],
   bootstrap: [AppComponent],
